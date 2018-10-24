bfc700c3b07eefd7821d825308c2ef5c14236e73
nmerrill67 <nmerrill@udel.edu>
2018-10-24 13:52:06 -0400

    rm tfrecord, made clear that advanced tutorial is not finished yet

--- code/tf_mnist_advanced.py
+++ code/tf_mnist_advanced.py
@@ -72,6 +72,9 @@ class Model:
 
 if __name__ == '__main__':
 
+    raise NotImplementedError("Advanced tutorial is not finished yet.")
+
+    
     (x_train, y_train),(x_test, y_test) = mnist.load_data()
     # Scale data  to [0,1]
     x_train, x_test = x_train / 255.0, x_test / 255.0
