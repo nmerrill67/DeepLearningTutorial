a45a9de7e5126b0edbfa60a5e08fbc9a64ee6ba4
Nate <nmerrill@udel.edu>
2019-04-30 00:35:16 -0400

    cleanup

--- code/vae_tutorial/figs/clean-eps.sh
+++
@@ -1,6 +0,0 @@
-#/bin/bash
-
-for file in *-eps-converted-to.pdf; do
-  mv "$file" "${file%-eps-converted-to.pdf}.pdf" # Rename pdf
-  rm "${file%-eps-converted-to.pdf}.eps" # remove stale eps file
-done
